8edead4907d7cd13f073fa127ba66b293e5681b2
Ally Greven <allysongreven@gmail.com>
2024-11-14 00:24:57 +0000

    unfinished issue-1-create-an-entry #1

--- ts/main.ts
+++ ts/main.ts
@@ -11,3 +11,95 @@ $photoInput.addEventListener('input', (event: Event) => {
   const newURL = input.value;
   $photoPreview.src = newURL;
 });
+
+/////////////////////////////////
+const $entryForm = document.querySelector('#entry-form') as HTMLFormElement;
+if (!$entryForm) throw new Error('$entryForm does not exist');
+
+$entryForm.addEventListener('submit', (event: Event) => {
+  event.preventDefault();
+
+for (let i = 0; i < $entryForm.elements.length; i++) {
+    const newEntry = $entryForm.elements[i] as HTMLInputElement;
+     if (newEntry.name) {
+      newEntry.name === newEntry.value;
+    }
+  }
+
+
+
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   // Assign entryId and increment nextEntryId
+//   newEntry.entryId = data.nextEntryId;
+//   data.nextEntryId++;
+
+//   // Add the new entry at the beginning of entries array
+//   data.entries.unshift(newEntry);
+
+//   // Reset the preview image to the placeholder
+//   $photoPreview.src = 'path/to/placeholder-image.jpg';
+
+//   // Reset the form fields
+//   $entryForm.reset();
+//   }
+
+// })
+
+
+
+
+
+
+// unshift
+
+
+
+// $entryForm.addEventListener('submit', (event: Event) => {
+//   event.preventDefault();
+//   const $formElements = $entryForm.elements as HTMLFormElement;
+// const entryData = {
+//   image: $formElements.url.value,
+//   title: $formElements.title.value,
+//   notes: $formElements.notes.value
+// }
+
+// })
+/*
+*
+
+
+
+
+
+8
+
+
+
+
+
+
+
+// for (let i = 0; i < $entryForm.elements.length; i++) {
+//   const element = $entryForm.elements[i] as HTMLInputElement;
+//   if (element.name === 'name' && element.value) {
+//     entryData.image = element.value;
+//   } else if (element.name === 'email' && element.value) {
+//     entryData.title = element.value;
+//   } else if (element.name === 'photo-url' && element.value) {
+//     entryData.notes = element.value;
+//   }
+// }
