2603a86848e27163ebe30d0704e99a79a239934f
Ally Greven <allysongreven@gmail.com>
2024-11-14 22:26:47 +0000

    function that reads data model assigned to data

--- ts/main.ts
+++ ts/main.ts
@@ -12,94 +12,35 @@ $photoInput.addEventListener('input', (event: Event) => {
   $photoPreview.src = newURL;
 });
 
-/////////////////////////////////
-const $entryForm = document.querySelector('#entry-form') as HTMLFormElement;
+/// //////////////////////////////
+interface formElements extends HTMLFormControlsCollection {
+  title: HTMLInputElement;
+  url: HTMLInputElement;
+  notes: HTMLTextAreaElement;
+}
+
+interface FormEntry {
+  title: string;
+  url: string;
+  notes: string;
+  entryId: number;
+}
+
+const $entryForm = document.querySelector('form');
 if (!$entryForm) throw new Error('$entryForm does not exist');
 
 $entryForm.addEventListener('submit', (event: Event) => {
   event.preventDefault();
-
-for (let i = 0; i < $entryForm.elements.length; i++) {
-    const newEntry = $entryForm.elements[i] as HTMLInputElement;
-     if (newEntry.name) {
-      newEntry.name === newEntry.value;
-    }
-  }
-
-
-
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   // Assign entryId and increment nextEntryId
-//   newEntry.entryId = data.nextEntryId;
-//   data.nextEntryId++;
-
-//   // Add the new entry at the beginning of entries array
-//   data.entries.unshift(newEntry);
-
-//   // Reset the preview image to the placeholder
-//   $photoPreview.src = 'path/to/placeholder-image.jpg';
-
-//   // Reset the form fields
-//   $entryForm.reset();
-//   }
-
-// })
-
-
-
-
-
-
-// unshift
-
-
-
-// $entryForm.addEventListener('submit', (event: Event) => {
-//   event.preventDefault();
-//   const $formElements = $entryForm.elements as HTMLFormElement;
-// const entryData = {
-//   image: $formElements.url.value,
-//   title: $formElements.title.value,
-//   notes: $formElements.notes.value
-// }
-
-// })
-/*
-*
-
-
-
-
-
-8
-
-
-
-
-
-
-
-// for (let i = 0; i < $entryForm.elements.length; i++) {
-//   const element = $entryForm.elements[i] as HTMLInputElement;
-//   if (element.name === 'name' && element.value) {
-//     entryData.image = element.value;
-//   } else if (element.name === 'email' && element.value) {
-//     entryData.title = element.value;
-//   } else if (element.name === 'photo-url' && element.value) {
-//     entryData.notes = element.value;
-//   }
-// }
+  const $formElements = $entryForm.elements as formElements;
+  const formData: FormEntry = {
+    entryId: data.nextEntryId,
+    title: $formElements.title.value,
+    url: $formElements.url.value,
+    notes: $formElements.notes.value,
+  };
+  data.nextEntryId++;
+  data.entries.unshift(formData);
+  $photoPreview.src = 'images/placeholder-image-square.jpg';
+  $entryForm.reset();
+  writeData();
+});
